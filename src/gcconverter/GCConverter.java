81c4abb7683e43d9dc91f54ca1ad3841992c6094
Milan Sulc <felix@noblexity.com>
2012-05-02 15:29:35 +0200

    Better Substance LaF settings

--- src/gcconverter/GCConverter.java
+++ src/gcconverter/GCConverter.java
@@ -1,31 +1,33 @@
 package gcconverter;
 
 import gcconverter.component.MainForm;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.UnsupportedLookAndFeelException;
-import org.pushingpixels.substance.api.skin.SubstanceModerateLookAndFeel;
+import org.ipx.player.component.MainForm;
+import org.pushingpixels.substance.api.skin.SubstanceBusinessBlackSteelLookAndFeel;
 
 /**
  *  Copyright 2012 Petr Stuchlik
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
  */
-
 /**
  *
  * @author stuchl4n3k
  */
 public class GCConverter {
-    
+
     public static final String CACHE_DETAILS_BASE_URL = "http://www.geocaching.com/seek/cache_details.aspx?wp=";
 
     /**
@@ -38,11 +40,13 @@ public class GCConverter {
             public void run() {
                 try {
                     //new org.pushingpixels.substance.api.skin.RavenSkin()
-                    javax.swing.UIManager.setLookAndFeel(new SubstanceModerateLookAndFeel());
+                    javax.swing.UIManager.setLookAndFeel(new SubstanceBusinessBlackSteelLookAndFeel());
+                    JFrame.setDefaultLookAndFeelDecorated(true);
+                    JDialog.setDefaultLookAndFeelDecorated(true);
                 } catch (UnsupportedLookAndFeelException ex) {
                     ex.printStackTrace();
                 }
-                
+
                 MainForm view = new MainForm();
                 //view.setExtendedState(JFrame.MAXIMIZED_BOTH);
                 view.setLocationByPlatform(true);
